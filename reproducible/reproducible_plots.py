7d1525bad458e40574106b8d140cc83fbe0ab058
sudoRicheek <richeekdas2001@gmail.com>
2021-10-18 19:02:19 +0530

    fixed plots

--- reproducible/reproducible_plots.py
+++ reproducible/reproducible_plots.py
@@ -10,8 +10,10 @@ import numpy as np
 from sklearn import metrics
 from sklearn.decomposition import KernelPCA, PCA
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 plt.rcParams.update({
     "text.usetex": True,
+    "hatch.linewidth": 1.5,
 })
 
 from utils.seed_handler import (
@@ -79,7 +81,7 @@ def case1and2(SAVE_PLOTS=False):
     colormap = np.array(['r', 'g', 'b', 'c', 'm', 'y'])
 
     plt.scatter(x, y, c=colormap[labels], alpha=0.8, marker='o', edgecolors='black', linewidths=0.75)
-    plt.title(r"KPCA of the "f"{V}"r" dimensional dataset with DEP-CON Kernel", fontsize=18)
+    plt.title(r"KPCA of the "f"{V}"r" dimensional data set with DEP-CON Kernel", fontsize=18)
     plt.xlabel(r'$\longleftarrow$ \textbf{X-component} $\longrightarrow$', fontsize=17)
     plt.ylabel(r'$\longleftarrow$ \textbf{Y-component} $\longrightarrow$', fontsize=17)
     plt.xticks(fontsize=14)
@@ -96,7 +98,7 @@ def case1and2(SAVE_PLOTS=False):
     y1 = linpca_2d[:,1]
 
     plt.scatter(x1, y1, c=colormap[labels], alpha=0.8, marker='o', edgecolors='black', linewidths=0.75)
-    plt.title(r'Linear PCA of the {} dimensional dataset'.format(V), fontsize=20)
+    plt.title(r'Linear PCA of the {} dimensional data set'.format(V), fontsize=20)
     plt.xlabel(r'$\longleftarrow$ \textbf{X-component} $\longrightarrow$', fontsize=18)
     plt.ylabel(r'$\longleftarrow$ \textbf{Y-component} $\longrightarrow$', fontsize=18)
     plt.xticks(fontsize=14)
@@ -172,36 +174,54 @@ Generates plot for the Linear Case.
 4 Histograms in a single figure with 
 # cluster counts
 """
-def case4(FILENAME, SAVE_PLOTS=False):
-    with open(os.path.join(__location__, '{}.json'.format(FILENAME))) as f:
-        data = json.load(f)
+def case4(F1, F2, SAVE_PLOTS=False):
+    with open(os.path.join(__location__, '{}.json'.format(F1))) as f:
+        data1 = json.load(f)
+    with open(os.path.join(__location__, '{}.json'.format(F2))) as f:
+        data2 = json.load(f)
 
-    print(data["name"])
+    print(data1["name"])
     print("==="*20)
 
-    number_of_runs = data["runs"]
-    cluster_pred_count = {method: {i: 0 for i in range(sl, su)} for method in method_names}
+    number_of_runs = data1["runs"]
+    cluster_pred_count_lin = {method: {i: 0 for i in range(sl, su)} for method in method_names}
+    cluster_pred_count_lincorr0 = {method: {i: 0 for i in range(sl, su)} for method in method_names}
 
     for method_name in method_names:
-        scores = np.array(data["cluster_predictions"][method_name])
+        scores = np.array(data1["cluster_predictions"][method_name])
         for clus in range(sl,su):
-            cluster_pred_count[method_name][clus] = np.count_nonzero(scores==clus)
+            cluster_pred_count_lin[method_name][clus] = np.count_nonzero(scores==clus)
+        
+        scores = np.array(data2["cluster_predictions"][method_name])
+        for clus in range(sl,su):
+            cluster_pred_count_lincorr0[method_name][clus] = np.count_nonzero(scores==clus)
 
 
     print("--- {}RUNS ---".format(number_of_runs))
     for method_name, verbose_name in zip(method_names, method_names_verbose):
-        print("({})Number of correct cluster predictions: ".format(verbose_name), cluster_pred_count[method_name])
+        print("({})Number of correct cluster predictions LINEAR: ".format(verbose_name), cluster_pred_count_lin[method_name])
+        print("---")
+    print("---")
+    for method_name, verbose_name in zip(method_names, method_names_verbose):
+        print("({})Number of correct cluster predictions LINEAR CORR ZERO: ".format(verbose_name), cluster_pred_count_lincorr0[method_name])
         print("---")
     print("---")
 
     
     # Number of correct cluster predictions
-    bars = dict()
+    bars1 = dict()
     for method_name in method_names:
         clus_temp = []
         for i in range(sl,su):
-            clus_temp+=[cluster_pred_count[method_name][i]]
-        bars[method_name] = clus_temp
+            clus_temp+=[cluster_pred_count_lin[method_name][i]]
+        bars1[method_name] = clus_temp
+    
+    bars2 = dict()
+    for method_name in method_names:
+        clus_temp = []
+        for i in range(sl,su):
+            clus_temp+=[cluster_pred_count_lincorr0[method_name][i]]
+        bars2[method_name] = clus_temp
 
 
     fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(9,8))
@@ -210,15 +230,29 @@ def case4(FILENAME, SAVE_PLOTS=False):
     barWidth = 0.3
     for i, row in enumerate(ax):
         for j, col in enumerate(row):
-            col.bar(range(sl,su), bars[method_names[2*i+j]], width=barWidth, color=color[2*i+j], alpha=0.7, edgecolor='black', linewidth=1.5, align='center', label=method_names_verbose[2*i+j])
+            col.bar(np.arange(sl,su)-barWidth/2, bars1[method_names[2*i+j]], width=barWidth, color=color[2*i+j], alpha=0.7, edgecolor='black', linewidth=1.5, align='center', label=method_names_verbose[2*i+j])
             col.legend(loc='upper right', fontsize=14)
-            col.set_ylim(top=30)
+            col.set_ylim(top=42)
             col.set_xticks(range(sl,su))
             col.tick_params(axis='both', which='major', labelsize=16)
-    fig.suptitle(r"Correct \# cluster predictions over 100 randomized runs.""\n"r" \textbf{Linear Case. True cluster count=6}", fontsize=22)
-    fig.supxlabel(r"\textbf{Cluster Search Range} $\longrightarrow$", fontsize=18)
+
+    for i, row in enumerate(ax):
+        for j, col in enumerate(row):
+            col.bar(np.arange(sl,su)+barWidth/2, bars2[method_names[2*i+j]], width=barWidth, color=color[2*i+j], alpha=0.7, edgecolor='black', linewidth=1.5, hatch="/", align='center', label=method_names_verbose[2*i+j])
+            col.set_ylim(top=42)
+            col.set_xticks(range(sl,su))
+            col.tick_params(axis='both', which='major', labelsize=16)
+
+    fig.suptitle(r"Correct \# cluster predictions over 100 randomized runs.""\n"r" \textbf{True cluster count=6}", fontsize=22)
+    fig.supxlabel(t=r"\textbf{Cluster Search Range} $\longrightarrow$", fontsize=18, y=0.07)
     fig.supylabel(r"\textbf{Number of times \# cluster predicted} $\longrightarrow$", fontsize=18)
-    fig.tight_layout()
+
+    solid_patch = mpatches.Patch(color='black', label=r"\textbf{Linear data set}")
+    striped_patch = mpatches.Patch(facecolor='white', edgecolor="black", linewidth=1.5, hatch="/", label=r"\textbf{Nonlinear data set}")
+    fig.legend(handles=[solid_patch, striped_patch], loc="lower center", fontsize=15, bbox_to_anchor=(0.47, -0.0), fancybox=True, ncol=5)
+    
+    fig.subplots_adjust(top=0.89, bottom=0.142, left=0.093, right=0.953, hspace=0.152, wspace=0.123)
+
     if SAVE_PLOTS:
         plt.savefig("numclus_preds{}.jpeg".format(number_of_runs), dpi=1000)
     plt.show()
@@ -264,27 +298,33 @@ def case5(F1, F2, SAVE_PLOTS=False):
     x = np.arange(len(method_names_verbose))  # the label locations
     barWidth = 0.25
     plt.figure(figsize=[8, 6])
+    color=['r','b','g','darkorchid']
 
     bar = []
     yerr = []
     for method_name in method_names:
         bar += [adj_rand_means1[method_name]]
         yerr += [error_bounds_adjrand1[method_name]]
-    plt.bar(x-barWidth/2, bar, width=barWidth, color='r', alpha=0.7, edgecolor='black', linewidth=1.5, yerr=yerr, capsize=7, align='center', label=r"\textbf{Linear Dataset}")
+    plt.bar(x-barWidth/2, bar, width=barWidth, color=color, alpha=0.7, edgecolor='black', linewidth=1.5, yerr=yerr, capsize=7, align='center', label=r"\textbf{Linear data set}")
 
     bar = []
     yerr = []
     for method_name in method_names:
         bar += [adj_rand_means2[method_name]]
         yerr += [error_bounds_adjrand2[method_name]]
-    plt.bar(x+barWidth/2, bar, width=barWidth, color='b', alpha=0.7, edgecolor='black', linewidth=1.5, yerr=yerr, capsize=7, align='center', label=r"\textbf{Lin. Corr. Zero Dataset}")
+    plt.bar(x+barWidth/2, bar, width=barWidth, color=color, alpha=0.7, edgecolor='black', linewidth=1.5, yerr=yerr, capsize=7, align='center', hatch="/", label=r"\textbf{Nonlinear data set}")
 
     plt.xticks(range(len(method_names)), method_names_verbose, fontsize=15)
     plt.title(r"Adjusted Rand Indices over {} randomized runs".format(number_of_runs), fontsize=22)
     plt.xlabel(r"\textbf{Clustering Algorithms} $\longrightarrow$", fontsize=18)
     plt.ylabel(r"\textbf{Adjusted Rand Indices} $\longrightarrow$", fontsize=18)
-    plt.legend(loc="lower center", fontsize=15)
     plt.yticks(fontsize=16)
+
+    solid_patch = mpatches.Patch(color='black', label=r"\textbf{Linear data set}")
+    striped_patch = mpatches.Patch(facecolor='white', edgecolor="black", linewidth=1.5, hatch="/", label=r"\textbf{Nonlinear data set}")
+    plt.legend(handles=[solid_patch, striped_patch], loc="lower center", fontsize=15, bbox_to_anchor=(0.47, -0.25), fancybox=True, ncol=5)
+    
+    plt.subplots_adjust(top=0.92, bottom=0.185, left=0.105, right=0.93, hspace=0.26, wspace=0.19)
 
     if SAVE_PLOTS:
         plt.savefig("adjrand_indices{}.jpeg".format(number_of_runs), dpi=1000)
@@ -299,7 +339,7 @@ if __name__=="__main__":
     elif t==3:
         case3(SAVE)
     elif t==4:
-        case4(FILENAME_LINEAR, SAVE)
+        case4(FILENAME_LINEAR, FILENAME_LINCORRZERO, SAVE)
     elif t==5:
         case5(FILENAME_LINEAR, FILENAME_LINCORRZERO, SAVE)
 
